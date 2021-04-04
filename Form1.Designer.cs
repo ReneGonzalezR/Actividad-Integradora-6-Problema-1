16d4f012d4e00a293f5e550486a0274a33409379
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-03 23:26:35 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿
+namespace Actividad_Integradora_6_Problema_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.listEstados = new System.Windows.Forms.ListBox();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.txtHabilidades = new System.Windows.Forms.TextBox();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            this.txtCiudad = new System.Windows.Forms.TextBox();
+            this.txtEstCivil = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.rbtModificacion = new System.Windows.Forms.RadioButton();
+            this.rbtBaja = new System.Windows.Forms.RadioButton();
+            this.rbtAlta = new System.Windows.Forms.RadioButton();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGuardar);
+            this.groupBox2.Controls.Add(this.listEstados);
+            this.groupBox2.Controls.Add(this.label23);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.label16);
+            this.groupBox2.Controls.Add(this.label17);
+            this.groupBox2.Controls.Add(this.label18);
+            this.groupBox2.Controls.Add(this.label19);
+            this.groupBox2.Controls.Add(this.label20);
+            this.groupBox2.Controls.Add(this.label21);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label22);
+            this.groupBox2.Controls.Add(this.txtHabilidades);
+            this.groupBox2.Controls.Add(this.txtEmpleado);
+            this.groupBox2.Controls.Add(this.txtCiudad);
+            this.groupBox2.Controls.Add(this.txtEstCivil);
+            this.groupBox2.Controls.Add(this.txtSexo);
+            this.groupBox2.Controls.Add(this.txtDireccion);
+            this.groupBox2.Controls.Add(this.txtAño);
+            this.groupBox2.Controls.Add(this.txtMes);
+            this.groupBox2.Controls.Add(this.txtDia);
+            this.groupBox2.Controls.Add(this.txtNombre);
+            this.groupBox2.Controls.Add(this.rbtModificacion);
+            this.groupBox2.Controls.Add(this.rbtBaja);
+            this.groupBox2.Controls.Add(this.rbtAlta);
+            this.groupBox2.Location = new System.Drawing.Point(82, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(757, 1023);
+            this.groupBox2.TabIndex = 35;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ABC Personal";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(277, 896);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(188, 58);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // listEstados
+            // 
+            this.listEstados.FormattingEnabled = true;
+            this.listEstados.ItemHeight = 41;
+            this.listEstados.Items.AddRange(new object[] {
+            "AGUASCALIENTES",
+            "BAJA CALIFORNIA",
+            "BAJA CALIFORNIA SUR",
+            "CAMPECHE",
+            "COAHUILA",
+            "COLIMA",
+            "CHIAPAS",
+            "CHIHUAHUA",
+            "CIUDAD DE MEXICO",
+            "DURANGO",
+            "GUANAJUATO",
+            "GUERRERO",
+            "HIDALGO",
+            "JALISCO",
+            "MEXICO",
+            "MICHOACAN",
+            "MORELOS",
+            "NAYARIT",
+            "NUEVO LEON",
+            "OAXACA",
+            "PUEBLA",
+            "QUERETARO",
+            "QUINTANA ROO",
+            "SAN LUIS POTOSI",
+            "SINALOA",
+            "SONORA",
+            "TABASCO",
+            "TAMAULIPAS",
+            "TLAXCALA",
+            "VERACRUZ",
+            "YUCATAN",
+            "ZACATECAS"});
+            this.listEstados.Location = new System.Drawing.Point(321, 689);
+            this.listEstados.Name = "listEstados";
+            this.listEstados.Size = new System.Drawing.Size(251, 45);
+            this.listEstados.TabIndex = 11;
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(-8, 268);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(297, 41);
+            this.label23.TabIndex = 35;
+            this.label23.Text = "Fecha de Nacimiento";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(126, 795);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(180, 41);
+            this.label13.TabIndex = 34;
+            this.label13.Text = "Habilidades:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(148, 742);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(158, 41);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "Empleado:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(192, 689);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(114, 41);
+            this.label15.TabIndex = 32;
+            this.label15.Text = "Estado:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(189, 636);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(119, 41);
+            this.label16.TabIndex = 31;
+            this.label16.Text = "Ciudad:";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(132, 583);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(176, 41);
+            this.label17.TabIndex = 30;
+            this.label17.Text = "Estado Civil:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(219, 522);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(89, 41);
+            this.label18.TabIndex = 29;
+            this.label18.Text = "Sexo:";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(159, 469);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(149, 41);
+            this.label19.TabIndex = 28;
+            this.label19.Text = "Direccion:";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(227, 416);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(79, 41);
+            this.label20.TabIndex = 27;
+            this.label20.Text = "Año:";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(227, 363);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(81, 41);
+            this.label21.TabIndex = 26;
+            this.label21.Text = "Mes:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(240, 309);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(68, 41);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Dia:";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(173, 217);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(135, 41);
+            this.label22.TabIndex = 24;
+            this.label22.Text = "Nombre:";
+            // 
+            // txtHabilidades
+            // 
+            this.txtHabilidades.Location = new System.Drawing.Point(322, 792);
+            this.txtHabilidades.Name = "txtHabilidades";
+            this.txtHabilidades.Size = new System.Drawing.Size(250, 47);
+            this.txtHabilidades.TabIndex = 13;
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Location = new System.Drawing.Point(322, 739);
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(250, 47);
+            this.txtEmpleado.TabIndex = 12;
+            // 
+            // txtCiudad
+            // 
+            this.txtCiudad.Location = new System.Drawing.Point(322, 633);
+            this.txtCiudad.Name = "txtCiudad";
+            this.txtCiudad.Size = new System.Drawing.Size(250, 47);
+            this.txtCiudad.TabIndex = 10;
+            // 
+            // txtEstCivil
+            // 
+            this.txtEstCivil.Location = new System.Drawing.Point(322, 580);
+            this.txtEstCivil.Name = "txtEstCivil";
+            this.txtEstCivil.Size = new System.Drawing.Size(250, 47);
+            this.txtEstCivil.TabIndex = 9;
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(322, 527);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(250, 47);
+            this.txtSexo.TabIndex = 8;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(322, 474);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(250, 47);
+            this.txtDireccion.TabIndex = 7;
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(322, 421);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(250, 47);
+            this.txtAño.TabIndex = 6;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(322, 368);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(250, 47);
+            this.txtMes.TabIndex = 5;
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(322, 314);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(250, 47);
+            this.txtDia.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(322, 214);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(250, 47);
+            this.txtNombre.TabIndex = 3;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // rbtModificacion
+            // 
+            this.rbtModificacion.AutoSize = true;
+            this.rbtModificacion.Location = new System.Drawing.Point(407, 97);
+            this.rbtModificacion.Name = "rbtModificacion";
+            this.rbtModificacion.Size = new System.Drawing.Size(197, 45);
+            this.rbtModificacion.TabIndex = 2;
+            this.rbtModificacion.Text = "Modificion";
+            this.rbtModificacion.UseVisualStyleBackColor = true;
+            this.rbtModificacion.CheckedChanged += new System.EventHandler(this.rbtModificacion_CheckedChanged);
+            // 
+            // rbtBaja
+            // 
+            this.rbtBaja.AutoSize = true;
+            this.rbtBaja.Location = new System.Drawing.Point(255, 97);
+            this.rbtBaja.Name = "rbtBaja";
+            this.rbtBaja.Size = new System.Drawing.Size(109, 45);
+            this.rbtBaja.TabIndex = 1;
+            this.rbtBaja.Text = "Baja";
+            this.rbtBaja.UseVisualStyleBackColor = true;
+            this.rbtBaja.CheckedChanged += new System.EventHandler(this.rbtBaja_CheckedChanged);
+            // 
+            // rbtAlta
+            // 
+            this.rbtAlta.AutoSize = true;
+            this.rbtAlta.Checked = true;
+            this.rbtAlta.Location = new System.Drawing.Point(88, 97);
+            this.rbtAlta.Name = "rbtAlta";
+            this.rbtAlta.Size = new System.Drawing.Size(106, 45);
+            this.rbtAlta.TabIndex = 0;
+            this.rbtAlta.TabStop = true;
+            this.rbtAlta.Text = "Alta";
+            this.rbtAlta.UseVisualStyleBackColor = true;
+            this.rbtAlta.CheckedChanged += new System.EventHandler(this.rbtAlta_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 1120);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 6";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ListBox listEstados;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.TextBox txtHabilidades;
+        private System.Windows.Forms.TextBox txtEmpleado;
+        private System.Windows.Forms.TextBox txtCiudad;
+        private System.Windows.Forms.TextBox txtEstCivil;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.RadioButton rbtModificacion;
+        private System.Windows.Forms.RadioButton rbtBaja;
+        private System.Windows.Forms.RadioButton rbtAlta;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+/// <summary>
+/// Lenguaje de programacion III
+/// Autor: Rene Gonzalez Rodriguez
+/// Maestro: Aarón I. Salazar
+/// </summary>
+namespace Actividad_Integradora_6_Problema_1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexion;
+        string query = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            conexion = new SqlConnection("server=RENEGONZALEZ\\SQLEXPRESS ; database=Personal ; integrated security = true");
+            conexion.Open();
+            rbtAlta.Focus();
+            txtDia.ReadOnly = false;
+            txtMes.ReadOnly = false;
+            txtAño.ReadOnly = false;
+            txtDireccion.ReadOnly = false;
+            txtSexo.ReadOnly = false;
+            txtEstCivil.ReadOnly = false;
+            txtCiudad.ReadOnly = false;
+            listEstados.SelectedItem = 0; ;
+            txtEmpleado.ReadOnly = false;
+            txtHabilidades.ReadOnly = false;
+            Limpiar();
+        }
+
+        private void rbtAlta_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDia.ReadOnly = false;
+            txtMes.ReadOnly = false;
+            txtAño.ReadOnly = false;
+            txtDireccion.ReadOnly = false;
+            txtSexo.ReadOnly = false;
+            txtEstCivil.ReadOnly = false;
+            txtCiudad.ReadOnly = false;
+            listEstados.SelectedItem = 0; ;
+            txtEmpleado.ReadOnly = false;
+            txtHabilidades.ReadOnly = false;
+            Limpiar();
+        }
+
+        private void rbtBaja_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDia.ReadOnly = true;
+            txtMes.ReadOnly = true;
+            txtAño.ReadOnly = true;
+            txtDireccion.ReadOnly = true;
+            txtSexo.ReadOnly = true;
+            txtEstCivil.ReadOnly = true;
+            txtCiudad.ReadOnly = true;
+            listEstados.SelectedItem = 0; ;
+            txtEmpleado.ReadOnly = true;
+            txtHabilidades.ReadOnly = true;
+            Limpiar();
+        }
+
+        private void rbtModificacion_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDia.ReadOnly = false;
+            txtMes.ReadOnly = false;
+            txtAño.ReadOnly = false;
+            txtDireccion.ReadOnly = false;
+            txtSexo.ReadOnly = false;
+            txtEstCivil.ReadOnly = false;
+            txtCiudad.ReadOnly = false;
+            listEstados.SelectedItem = 0; ;
+            txtEmpleado.ReadOnly = false;
+            txtHabilidades.ReadOnly = false;
+            Limpiar();
+        }
+        public void Limpiar()
+        {
+            txtNombre.Text = string.Empty;
+            txtDia.Text = string.Empty;
+            txtMes.Text = string.Empty;
+            txtAño.Text = string.Empty;
+            txtDireccion.Text = string.Empty;
+            txtSexo.Text = string.Empty;
+            txtEstCivil.Text = string.Empty;
+            txtCiudad.Text = string.Empty;
+            listEstados.SelectedItem = 0; ;
+            txtEmpleado.Text = string.Empty;
+            txtHabilidades.Text = string.Empty;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (rbtAlta.Checked)
+            {
+                query = string.Format("INSERT INTO persona VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')",
+                    txtNombre.Text, txtDia.Text, txtMes.Text, txtAño.Text, txtDireccion.Text, txtSexo.Text, txtEstCivil.Text, txtCiudad.Text,
+                    listEstados.Text, txtEmpleado.Text, txtHabilidades.Text);
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    string dato = string.Format("Error al realizar la insercion: {0}", ex.Message);
+                    MessageBox.Show(dato, "Error");
+                }
+            }
+            if (rbtBaja.Checked)
+            {
+                query = string.Format("DELETE FROM persona WHERE nombre = '{0}'",
+                   txtNombre.Text);
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    string dato = string.Format("Error al realizar la Eliminacion: {0}", ex.Message);
+                    MessageBox.Show(dato, "Error");
+                }
+            }
+            if (rbtModificacion.Checked)
+            {
+                query = string.Format("UPDATE persona SET dianacimiento ='{1}',mesnacimiento = '{2}', añonacimiento= '{3}', direccion ='{4}'," +
+                    " sexo = '{5}', estadocivil = '{6}', ciudad ='{7}', estado = '{8}', empleado = '{9}', habilidades ='{10}' WHERE nombre = '{0}'",
+                   txtNombre.Text, txtDia.Text, txtMes.Text, txtAño.Text, txtDireccion.Text, txtSexo.Text, txtEstCivil.Text, txtCiudad.Text,
+                   listEstados.Text, txtEmpleado.Text, txtHabilidades.Text);
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    string dato = string.Format("Error al realizar la insercion: {0}", ex.Message);
+                    MessageBox.Show(dato, "Error");
+                }
+            }
+            MessageBox.Show("Exito", "Mensaje");
+            Limpiar();
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                query = string.Format("select dianacimiento, mesnacimiento, añonacimiento, direccion, sexo, estadocivil, ciudad, " +
+                    "estado, empleado, habilidades  from persona WHERE nombre = '{0}'", txtNombre.Text);
+                SqlCommand cmd = new SqlCommand( query, conexion);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txtDia.Text = dr["dianacimiento"].ToString();
+                    txtMes.Text = dr["mesnacimiento"].ToString();
+                    txtAño.Text = dr["añonacimiento"].ToString();
+                    txtDireccion.Text = dr["direccion"].ToString();
+                    txtSexo.Text = dr["sexo"].ToString();
+                    txtEstCivil.Text = dr["estadocivil"].ToString();
+                    txtCiudad.Text = dr["ciudad"].ToString();
+                    listEstados.Text = dr["estado"].ToString();
+                    txtEmpleado.Text = dr["empleado"].ToString();
+                    txtHabilidades.Text = dr["habilidades"].ToString();
+                }
+                dr.Close();
+            }
+        }
+    }
+}
